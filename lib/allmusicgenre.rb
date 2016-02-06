d96134af5d61064b7877e758b90592284def84d5
Richard Lyon <richard@richardlyon.net>
2016-02-06 22:11:14 +0000

    Documentation

--- lib/allmusicgenre.rb
+++ lib/allmusicgenre.rb
@@ -1,29 +1,33 @@
-# Client for scraping Allmusic.com
-
 require 'nokogiri'
 require 'uri'
 require 'open-uri'
 require 'fuzzystringmatch' #https://github.com/kiyoka/fuzzy-string-match
 
+# Allmusic.com client
+# @param [String]
 class Allmusic
 
-  attr_accessor :album, :artist
-  attr_reader   :genre, :style
+  # @!attribute album
+  #   @return [String] Album name
+  attr_accessor :album
+  # @!attribute artist
+  #   @return [String] Artist name
+  attr_accessor :artist
+  attr_reader   :genre
+  attr_reader   :style
 
-  ARTIST_SEARCH = "http://www.allmusic.com/search/artists/"
-  ALBUM_SEARCH = "http://www.allmusic.com/search/albums/"
+  ARTIST_SEARCH_URL = "http://www.allmusic.com/search/artists/"
 
   def initialize( artist = nil, album = nil)
     @artist = artist
-    @album = album
+    @album  = album
   end
 
-  # Sets genre and style for album, artist
+  # Sets @genre and @style for @album, @artist
   def get_meta
 
-
     # search for artist page e.g. http://www.allmusic.com/search/artists/abba
-    artist_search_url = make_url(ARTIST_SEARCH, @artist)
+    artist_search_url = make_url(ARTIST_SEARCH_URL, @artist)
     artist_search_page = Nokogiri::HTML(open(artist_search_url))
 
     if no_search_result?(artist_search_page)
@@ -55,23 +59,24 @@ class Allmusic
     @style = album_page.xpath("//div[@class='styles']//a[1]").text
   end
 
-  # return a clean, joined url
-  # TODO: unsafe, not portable - File.join gives the wrong separator on windows
-  # TODO: escape url
+  # @return [URL] Joins URL parts
   def make_url( root, path)
+    # TODO: unsafe, not portable - File.join gives the wrong separator on windows
+    # TODO: escape url
     clean_url = File.join(root, path)
-
     return clean_url
   end
 
+  # @return [true, false] Returns `true` if no search results for the given page
   def no_search_result?(page)
     return !page.xpath('//div[@class="no-results"]').empty?
   end
 
-  # return the closest match to 'target' from list 'candidates'
-  # 'closest' is highest Jaro-Winkler distance
-  # TODO: replace with a proper array sort
+  # @return [URL] The candidate with the highest Jaro-Winkler distance from target
+  # @param target [String] the string to compare
+  # @param candidates [Array] array of Nokogiri <A> nodes
   def best_match(target, candidates)
+    # TODO: replace with a proper array sort
     confidence = 0
     best_url = nil
     jarow = FuzzyStringMatch::JaroWinkler.create( :native )
